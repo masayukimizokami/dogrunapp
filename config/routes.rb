9625bed34b068c57e4ed411b46c06c801132fa09
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-02-22 14:29:14 +0900

    root追加

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,6 @@ Rails.application.routes.draw do
     end
   end
   root to: 'dogruns#top'
-
+  root  'dogruns#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
