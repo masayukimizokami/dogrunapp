cd30a18401ab372b7a12d261364fa13c7ace850e
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-02-27 05:48:22 +0900

    一時的にゲスト変更可能

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,7 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :check_guest, only: %i[update destroy]
+ # before_action :check_guest, only: %i[update destroy]
 
-  def check_guest
-    redirect_to root_path, alert: 'ゲストユーザーの編集・削除できません.' if resource.email == 'guest@example.com'
-  end
+ # def check_guest
+   # redirect_to root_path, alert: 'ゲストユーザーの編集・削除できません.' if resource.email == 'guest@example.com'
+ # end
 end
