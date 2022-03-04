a09391bbf3d3c5350b90ba5d5819d95878c89721
溝上正幸 <mizokamimasayuki@mizokamimasayukinoMacBook-Air.local>
2022-03-04 10:02:50 +0900

    guestログイン調整

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,5 @@ class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 
   def check_guest
-    redirect_to root_path, alert: 'ゲストユーザーの編集・削除できません.' if resource.email == 'guest@example.com'
   end
 end
